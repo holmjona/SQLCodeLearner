c817353aaa2b9d9f0a788ae3580f3541742d31d2
Jon Holmes <holmjona@isu.edu>
2018-10-25 12:23:32 -0600

    Added Data Access Layer

---
+++ CodeLearner/CodeLearner/DAL.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLearner {
+    static class DAL {
+        private static string _ConnectionString = null;
+
+        private static string ConnectionString {
+            get {
+                if (_ConnectionString == null) {
+                    //the next three lines of code are to allow for relative paths 
+                    // and is based on code found at:
+                    // https://stackoverflow.com/questions/1833640/connection-string-with-relative-path-to-the-database-file
+                    string exeLoc = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                    string path = (System.IO.Path.GetDirectoryName(exeLoc));
+                    AppDomain.CurrentDomain.SetData("DataDirectory", path);
+                    _ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\data\MyData.mdf;Integrated Security=True";
+                }
+                return _ConnectionString;
+            }
+        }
+
+        public static List<Person> GetPeople() {
+            List<Person> peops = new List<Person>();
+            SqlConnection conn = null;
+            try {
+                conn = new SqlConnection(ConnectionString);
+                conn.Open();
+                SqlCommand comm =
+                    new SqlCommand("SELECT * FROM People");
+                comm.Connection = conn;
+
+                SqlDataReader dr = comm.ExecuteReader();
+                while (dr.Read()) {
+                    Person p = new Person();
+                    p.ID = (int)dr["PersonID"];
+                    p.FirstName = (String)dr["FirstName"];
+                    p.LastName = (String)dr["LastName"];
+                    p.DateOfBirth = (DateTime)dr["DateOfBirth"];
+                    p.IsManager = (bool)dr["IsManager"];
+                    p.Email = (String)dr["Email"];
+                    p.Phone = (string)dr["Phone"];
+                    p.PreFix = (string)dr["PreFix"];
+                    p.PostFix = (string)dr["PostFix"];
+                    p.HomePage = (String)dr["HomePage"];
+                    peops.Add(p);
+                }
+            } catch (Exception ex) {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            } finally {
+                if (conn != null) conn.Close();
+            }
+            return peops;
+        }
+
+        public static Person GetPerson(int id) {
+            Person retPerson = null;
+            SqlConnection conn = null;
+            try {
+                conn = new SqlConnection(ConnectionString);
+                conn.Open();
+                //SqlCommand comm =
+                //    new SqlCommand("SELECT * FROM People WHERE PersonID = id"
+                //    , conn);
+
+                String qry = "SELECT * FROM People WHERE PersonID = @ID";
+                SqlCommand comm = new SqlCommand(qry, conn);
+                comm.Parameters.AddWithValue("@ID", id);
+
+                //comm.Connection = conn;
+
+                SqlDataReader dr = comm.ExecuteReader();
+                while (dr.Read()) {
+                    retPerson = new Person();
+                    retPerson.ID = (int)dr["PersonID"];
+                    retPerson.FirstName = (String)dr["FirstName"];
+                    retPerson.LastName = (String)dr["LastName"];
+                    retPerson.DateOfBirth = (DateTime)dr["DateOfBirth"];
+                    retPerson.IsManager = (bool)dr["IsManager"];
+                    retPerson.Email = (String)dr["Email"];
+                    retPerson.Phone = (string)dr["Phone"];
+                    retPerson.PreFix = (string)dr["PreFix"];
+                    retPerson.PostFix = (string)dr["PostFix"];
+                    retPerson.HomePage = (String)dr["HomePage"];
+                }
+            } catch(Exception ex) { 
+            } finally {
+                if (conn != null) conn.Close();
+            }
+
+
+            return retPerson;
+        }
+
+
+
+    }
+}
--- CodeLearner/CodeLearner/MainWindow.xaml.cs
+++ CodeLearner/CodeLearner/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace CodeLearner {
         }
 
         private void btnGetNames_Click(object sender, RoutedEventArgs e) {
-            List<Person> peops = GetPeopleFromDatabase();
+            List<Person> peops = DAL.GetPeople(); //GetPeopleFromDatabase();
 
             foreach (Person p in peops) {
                 stkNames.Children.Add(new Label() {
@@ -36,39 +36,46 @@ namespace CodeLearner {
 
         }
 
-        private List<Person> GetPeopleFromDatabase() {
-            List<Person> peops = new List<Person>();
-            SqlConnection conn = null;
-            try {
-                //the next three lines of code are to allow for relative paths 
-                // and is based on code found at:
-                // https://stackoverflow.com/questions/1833640/connection-string-with-relative-path-to-the-database-file
-                string exeLoc = System.Reflection.Assembly.GetExecutingAssembly().Location;
-                string path = (System.IO.Path.GetDirectoryName(exeLoc));
-                AppDomain.CurrentDomain.SetData("DataDirectory", path);
-
-                conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\data\MyData.mdf;Integrated Security=True");
-                conn.Open();
-                //MessageBox.Show("Dude, we're in.");
-                SqlCommand comm =
-                    new SqlCommand("SELECT  * FROM People");
-                comm.Connection = conn;
-
-                SqlDataReader dr = comm.ExecuteReader();
-                while (dr.Read()) {
-                    Person p = new Person();
-                    p.FirstName = (String)dr["FirstName"];
-                    p.LastName = (String)dr["LastName"];
-                    peops.Add(p);
-                }
-
-
-            } catch (Exception ex) {
-                MessageBox.Show("Oops; " + ex.Message);
-            } finally {
-                if (conn != null) conn.Close();
-            }
-            return peops;
+        private void btnGetPerson_Click(object sender, RoutedEventArgs e) {
+            int pID = int.Parse(txtPersonID.Text);
+            Person p = DAL.GetPerson(pID);
+            MessageBox.Show(p.FirstName 
+                + " - Is manager: " + (p.IsManager ? "Yes": "No"));
         }
+
+        //private List<Person> GetPeopleFromDatabase() {
+        //    List<Person> peops = new List<Person>();
+        //    SqlConnection conn = null;
+        //    try {
+        //        //the next three lines of code are to allow for relative paths 
+        //        // and is based on code found at:
+        //        // https://stackoverflow.com/questions/1833640/connection-string-with-relative-path-to-the-database-file
+        //        string exeLoc = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        //        string path = (System.IO.Path.GetDirectoryName(exeLoc));
+        //        AppDomain.CurrentDomain.SetData("DataDirectory", path);
+
+        //        conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\data\MyData.mdf;Integrated Security=True");
+        //        conn.Open();
+        //        //MessageBox.Show("Dude, we're in.");
+        //        SqlCommand comm =
+        //            new SqlCommand("SELECT  * FROM People");
+        //        comm.Connection = conn;
+
+        //        SqlDataReader dr = comm.ExecuteReader();
+        //        while (dr.Read()) {
+        //            Person p = new Person();
+        //            p.FirstName = (String)dr["FirstName"];
+        //            p.LastName = (String)dr["LastName"];
+        //            peops.Add(p);
+        //        }
+
+
+        //    } catch (Exception ex) {
+        //        MessageBox.Show("Oops; " + ex.Message);
+        //    } finally {
+        //        if (conn != null) conn.Close();
+        //    }
+        //    return peops;
+        //}
     }
 }
